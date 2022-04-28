cffe8f935e25ee6431b23d271843a5ad3d90e0c3
GFedorov <32594038+GFedorov@users.noreply.github.com>
2022-04-28 10:01:34 +0300

    express

--- app.js
+++ app.js
@@ -1,40 +1,112 @@
-// server.js
 require('dotenv').config();
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
 
-// const dev = process.env.NODE_ENV !== 'production'
-const dev = false;
-const hostname = 'localhost';
-const port = process.env.PORT || 3000
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const port = process.evn.PORT || 3000
 
-app.prepare().then(() => {
-    createServer(async (req, res) => {
-        try {
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true)
-            const { pathname, query } = parsedUrl
 
-            // if (pathname === '/a') {
-            //     await app.render(req, res, '/a', query)
-            // } else if (pathname === '/b') {
-            //     await app.render(req, res, '/b', query)
-            // } else
-            //  {
-            await handle(req, res, parsedUrl)
-            // }
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err)
-            res.statusCode = 500
-            res.end('internal server error')
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+const api = new WooCommerceRestApi({
+    url: process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL,
+    consumerKey: process.env.WC_CONSUMER_KEY,
+    consumerSecret: process.env.WC_CONSUMER_SECRET,
+    version: "wc/v3"
+});
+
+let updateTime = 0;
+let cache = {};
+const updateCache = async () => {
+    let newCache = {};
+    // получить список продуктов по SKU
+    const { data } = await api.get(
+        'products',
+        {
+            per_page: 50,
+            sku: 'ДС.060087,ДС.060092,ДС.060094,ДС.060095,ДС.060096,ДС.060098,ДС.060100,ДС.060101,ДС.060102,ДС.060104,ДС.060106,ДС.060129,ДС.060137,ДС.070951,ДС.070956,ДС.070957,ДС.071355,ДС.071380,ДС.071384,ДС.071643,ДС.070951,ДС.070956,ДС.070957,ДС.071355'
         }
-    }).listen(port, (err) => {
-        if (err) throw err
-        console.log(`> Ready on http://${hostname}:${port}`)
-    })
+
+    );
+
+    //составить  список ID продуктов
+
+    const productDictionary = {};
+    const productIds = [];
+    const variationDictionary = {};
+
+    for (let product of data) {
+        productDictionary[product.id] = product;
+        productIds.push(product.id);
+    }
+    //получить варианты по ID продукта в цикле
+    for (let productId of productIds) {
+        const { data } = await api.get(
+            `products/${productId}/variations`,
+            {
+                per_page: 50
+            }
+        );
+
+        for (let variation of data) {
+            variationDictionary[variation.id] = { ...variation, productId: productId };
+
+        }
+
+    }
+
+    const d = {};
+    for (let variantId in variationDictionary) {
+        const variation = variationDictionary[variantId];
+        const product = productDictionary[variation.productId];
+        let attributes = variation?.attributes[0]?.option || '';
+        if (!attributes.match(/[0-9]+/gm)) {
+            attributes = null;
+        }
+
+
+        d[variantId] = {
+            id: variantId,
+            name: product.name + (attributes ? ` (${attributes})` : ''),
+            img: variation?.image[0]?.src || product?.images[0]?.src,
+            link: variation?.permalink || product?.permalink,
+            price: variation?.price || product?.price,
+            attributes: attributes,
+        };
+
+    }
+
+    //составить словарь из продуктов и вариантов с Id варианта
+    updateTime = Date.now();
+    newCache = {
+        d: d,
+        updateTime: updateTime
+
+    };
+    //записать в кеш
+    cache = newCache;
+
+
+}
+updateCache();
+
+app.use(cors());
+
+app.get('/api/get-products', (req, res) => {
+    try {
+        // проверить время обновления кеша
+        if (Date.now() - updateTime > 1000 * 60 * 60) {
+            await updateCache();
+        }
+        // если время обновления кеша больше 60 мин назад, то обновить кеш
+        res.json(cache);
+        //вернуть кеш
+    } catch (e) {
+        console.log(e)
+    }
+    return;
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
